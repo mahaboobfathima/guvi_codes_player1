d0c507d1656064ed5202f05dbc533d084a1f5ddc
mithun9421 <mithunmuralee94@gmail.com>
2016-08-26 14:06:00 +0530

    Create revanum.java

---
+++ revanum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+ 
+class Reverse
+{
+   private static Scanner in;
+
+public static void main(String args[])
+   {
+      int n, rev = 0;
+ 
+     
+      in = new Scanner(System.in);
+      n = in.nextInt();
+ 
+      while( n != 0 )
+      {
+          rev = rev * 10;
+          rev = rev + n%10;
+          n = n/10;
+      }
+ 
+      System.out.println("Reverse of entered number is "+rev);
+   }
+}
